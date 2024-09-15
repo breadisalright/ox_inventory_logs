490371258d61af566b5a357af21ae333ecb9595a
breadisalright <153122702+breadisalright@users.noreply.github.com>
2024-09-15 19:28:53 +0100

    Ox-Webhook

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Bread'
+description 'Webhook for ox_inventory'
+
+server_scripts {
+    'configs/sv_logs.lua',
+    'server/sv_oxhook.lua',
+    'server/sv_hooks.lua'
+}
